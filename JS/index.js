d6587cc7b3dceec1d6426185bb955c43ddbe53e7
Elian Trupiano <eliantrupianogg@gmail.com>
2024-05-26 19:04:44 -0300

    Enlazar HTML con JS

---
+++ JS/index.js
@@ -0,0 +1,12 @@
+/* Algoritmo con condicional */
+
+let valorIngresado = true
+
+while (valorIngresado){
+    let numero = prompt("Ingresa tu valor")
+}if(valorIngresado){
+    let elResultado = (numero * 1.62)
+    alert(`El valor total es $${elResultado}`)
+}
+
+// ACLARACION para agregar el .toFixed, tuve que investigar ya que me daban demasiados decimales en el resultado 
